e90bcd5630b5994158df2161ab001499abade5fa
hemant <hemvish11@gmail.com>
2024-05-06 13:38:48 +0530

    more canvases reduced

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import { BallCanvas } from './canvas';
 import { SectionWrapper } from '../hoc';
-import { technologies,technologiesMobile } from '../constants';
+import { technologies, technologiesMobile } from '../constants';
 
 const Tech = () => {
   return (
     <>
-    <div className='lg:flex lg:flex-wrap justify-center gap-10 hidden lg:visible'>
-      {technologies.map((technology)=>(
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon}/>
-        </div>
-      ))}
-    </div>
-    <div className='flex flex-wrap justify-center gap-10 lg:hidden'>
-      {technologiesMobile.map((technology)=>(
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon}/>
-        </div>
-      ))}
-    </div>
+      <div className='lg:flex lg:flex-wrap justify-center gap-10 hidden lg:visible'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+      <div className='flex flex-wrap justify-center gap-10 lg:hidden'>
+        {technologiesMobile.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
     </>
   )
 }
 
-export default SectionWrapper(Tech,"");
+export default SectionWrapper(Tech, "");
